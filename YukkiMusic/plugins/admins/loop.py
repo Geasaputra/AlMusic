d34c25aeab0bed65993b19c5fd95574a10bcfff6
Geasaputra <108199249+Geasaputra@users.noreply.github.com>
2024-01-20 01:42:14 +0700

    Update loop.py

--- YukkiMusic/plugins/admins/loop.py
+++ YukkiMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from YukkiMusic import app
+from YukkiMusic.utils.database import get_loop, set_loop
+from YukkiMusic.utils.decorators import AdminRightsCheck
+from YukkiMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
